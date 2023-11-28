df080c0e0d6bb1789136f284ec0b12b1accc3b5e
Rohith2703 <sairohith28@gmail.com>
2023-11-28 19:22:46 +0530

    adding terraform file

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1a"
+  access_key = "AKIATKSVVGGA2I3E3UOR"
+  secret_key = "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"
+}
+
+resource "aws_instance" "test-server" {
+  ami           = "ami-0287a05f0ef0e9d9a"  
+  instance_type = "t2.micro"
+}
