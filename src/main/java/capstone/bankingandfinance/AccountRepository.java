cd3e36ddf208ad2c2daf2eb1757cecd637f13f73
Rohith2703 <sairohith28@gmail.com>
2023-11-28 12:35:00 +0530

    adding banking app files

---
+++ src/main/java/capstone/bankingandfinance/AccountRepository.java
@@ -0,0 +1,7 @@
+package capstone.bankingandfinance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Integer>{
+
+}
---
+++ src/test/java/capstone/bankingandfinance/BankingandfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.bankingandfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingandfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
