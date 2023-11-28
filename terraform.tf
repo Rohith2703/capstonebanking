f6245eb8b9fcef7a1c7b3e6efaf7fe147405c8c8
Rohith2703 <sairohith28@gmail.com>
2023-11-28 20:06:10 +0530

    adding terraform file

--- terraform.tf
+++ terraform.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1a"
-  access_key = "AKIATKSVVGGA2I3E3UOR"
-  secret_key = "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"
 }
 
 resource "aws_instance" "test-server" {
