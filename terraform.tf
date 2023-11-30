e025a65509e4382d2e73410e7df6b7065de21c0e
Rohith2703 <sairohith28@gmail.com>
2023-11-30 18:13:02 +0530

    editing terraform file

--- terraform.tf
+++ terraform.tf
@@ -19,7 +19,18 @@ resource "aws_instance" "test-server" {
      Name = "Test-Server"
   }
 }
+resource "aws_instance" "prod-server" {
+  ami           = "ami-0287a05f0ef0e9d9a"  
+  instance_type = "t2.micro"
+  tags = {
+     Name = "Prod-Server"
+  }
+}
 
 output "test_server_ip" {
   value = aws_instance.test-server.public_ip
+}
+
+output "prod_server_ip" {
+  value = aws_instance.prod-server.public_ip
 }
